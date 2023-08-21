b738cb372ff76a88fcd4f19ef08ac1bfc359121c
Milton <miltonmjcp@gmail.com>
2023-08-20 23:12:48 -0300

    chore: configurações

--- src/main/java/com/movies/ranking/config/SecurityConfig.java
+++ src/main/java/com/movies/ranking/config/SecurityConfig.java
@@ -11,19 +11,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 @Log4j2
-//Habilita a validação em métodos globlemente
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    /**
-     * BasicAutheticationFilter -> Autentica se temos uma autorização em cima de info. base64
-     * UsernamePasswordAuthenticationFilter -> Autentica nome e senha
-     * DefaultLoginPageGeneratingFilter -> Pagina de login padrão
-     * DefaultLogoutPageGeneratingFilter -> Pagina de logout padrão
-     * FilterSecurityInterceptor -> Autentica se estou autorizado
-     * Authentication -> Autorization
-     * http://localhost:8080/login -> Aparecerá a tela para login
-     * http://localhost:8080/logout -> Aoarecerpa a tela para logout
-     */
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         //Desabilita o token
